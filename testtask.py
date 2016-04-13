cd2388143a18b8ddc6107b64aeb000127e12db55
alexeyrud <rud.alexey@gmail.com>
2016-04-13 22:37:42 +0300

    Fix bug with first position biggest number

--- testtask.py
+++ testtask.py
@@ -3,7 +3,7 @@
 с неуникальными элементами
 """
 
-l = [3, 4, 2, 6, 7, 7, 3]
+l = [8, 8, 3, 4, 2, 6, 7, 7, 3]
 Big1 = 0
 Big2 = 0
 
@@ -11,5 +11,13 @@ for i in l:
     if Big1 < i:
         Big2 = Big1
         Big1 = i
+
+if Big2 == Big1:
+    Big2 = 0
+
+if Big2 == 0:
+    for i in l:
+        if Big2 < i and i != Big1:
+            Big2 = i
 
 print(Big1, Big2)
